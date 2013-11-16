9be8e50792011d35978d269bf5c7acb91bbdd9d9
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-11-16 18:20:24 +0000

    Added Event Store connection

---
+++ src/TwitterQuiz/EventStore/EventStoreConnectionProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using EventStore.ClientAPI;
+
+namespace TwitterQuiz.EventStore
+{
+    public class EventStoreConnectionProvider
+    {
+        private static IEventStoreConnection _eventStoreConnection;
+
+        public static IEventStoreConnection EventStore
+        {
+            get { return _eventStoreConnection ?? (_eventStoreConnection = CreateEventStoreConnection()); }
+        }
+
+        private static IEventStoreConnection CreateEventStoreConnection()
+        {
+            var ip = IPAddress.Parse(ConfigurationManager.AppSettings["Store.IP"]);
+            int tcpPort = Convert.ToInt32(ConfigurationManager.AppSettings["Store.Port"]);
+            var tcpEndPoint = new IPEndPoint(ip, tcpPort);
+
+            return EventStoreConnection.Create(ConnectionSettings.Default, tcpEndPoint);
+        }
+    }
+}
---
+++ src/TwitterQuiz/EventStore/EventStoreNinjectModule.cs
@@ -0,0 +1,15 @@
+﻿using EventStore.ClientAPI;
+using Ninject.Modules;
+
+namespace TwitterQuiz.EventStore
+{
+    public class EventStoreNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IEventStoreConnection>()
+                .ToMethod(x => EventStoreConnectionProvider.EventStore)
+                .InSingletonScope();
+        }
+    }
+}
