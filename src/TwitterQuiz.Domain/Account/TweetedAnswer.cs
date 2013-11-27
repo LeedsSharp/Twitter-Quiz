4ec527a32add788b5589297908d1e8907d1a5cdb
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-11-27 12:58:23 +0000

    added tweet classes

---
+++ src/TwitterQuiz.Domain/Account/TweetedAnswer.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitterQuiz.Domain.Account
+{
+    public class TweetedAnswer
+    {
+        public Host Host { get; set; }
+        public int Round { get; set; }
+        public int Question { get; set; }
+        public string QuizName { get; set; }
+        public string Tweet { get; set; }
+    }
+}
