e58fd01d13375e6b6de6970b2bcabaca00253e75
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-12-09 14:17:08 +0000

    added RavenDb references

---
+++ src/TwitterQuiz/Ninject/RavenDbNinjectModule.cs
@@ -0,0 +1,19 @@
+﻿using Ninject.Modules;
+using Raven.Client;
+
+namespace TwitterQuiz.Ninject
+{
+    public class RavenDbNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDocumentSession>()
+                .ToMethod(x =>
+                {
+                    var store = RavenSessionProvider.DocumentStore;
+                    return store.OpenSession();
+                })
+                .InSingletonScope();
+        }
+    }
+}
---
+++ src/TwitterQuiz/RavenSessionProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using Raven.Client.Document;
+
+namespace TwitterQuiz
+{
+    public class RavenSessionProvider
+    {
+        private static DocumentStore _documentStore;
+
+        public bool SessionInitialized { get; set; }
+
+        public static DocumentStore DocumentStore
+        {
+            get { return (_documentStore ?? (_documentStore = CreateDocumentStore())); }
+        }
+
+        private static DocumentStore CreateDocumentStore()
+        {
+            var store = new DocumentStore
+            {
+                Url = ConfigurationManager.AppSettings["RAVEN_URI"],
+                DefaultDatabase = ConfigurationManager.AppSettings["RAVEN_Database"],
+                ApiKey = ConfigurationManager.AppSettings["RAVEN_APIKEY"]
+            };
+            store.Initialize();
+
+            return store;
+        }
+    }
+}
