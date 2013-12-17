5780c9d5ee7096a10c1a83aa40ae70df9bd45df6
Matt Ross <matt@tronik.co.uk>
2013-12-17 15:37:06 +0000

    Can tweet a question

---
+++ src/TwitterQuiz.AppServices/TweetService.cs
@@ -0,0 +1,25 @@
+﻿using TweetSharp;
+
+namespace TwitterQuiz.AppServices
+{
+    public class TweetService
+    {
+        private readonly TwitterService twitterService;
+        private const string ConsumerKey = "cCUfLlJdwNanXcdTFPaw";
+        private const string ConsumerSecret = "LeLyBeYm655HdtdAJnBr74NDZ7DXWKgxmVQomhN1Y";
+        private const string AccessToken = "576199065-QkMWPvETEvstaTkC2ksQ8Y3tvrJip0XDjTVgEZu7";
+        private const string AccessTokenSecret = "ydDC3XFV98i8GHihwbMkhD5VNGrbtRE0B0iQM6Mtj0nAj";
+
+        public TweetService()
+        {
+            var twitterClientInfo = new TwitterClientInfo { ConsumerKey = ConsumerKey, ConsumerSecret = ConsumerSecret };
+            twitterService = new TwitterService(twitterClientInfo);
+        }
+
+        public void Tweet(string message)
+        {
+            twitterService.AuthenticateWith(AccessToken, AccessTokenSecret);
+            twitterService.SendTweet(new SendTweetOptions { Status = message });
+        }
+    }
+}
---
+++ src/TwitterQuiz.Test/Twitter/TweetServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using TwitterQuiz.AppServices;
+
+namespace TwitterQuiz.Test.Twitter
+{
+    [TestFixture]
+    public class TweetServiceTests
+    {
+        #region Setup
+        [TestFixtureSetUp]
+        public void SetUpOnce()
+        {
+
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownOnce()
+        {
+
+        }
+
+        [SetUp]
+        public void SetupBeforeEveryTest()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDownBeforeEveryTest()
+        {
+
+        } 
+        #endregion
+
+        [Test]
+        public void Can_tweet_a_question()
+        {
+            // Arrange
+            var tweetService = new TweetService();
+
+            // Act
+            tweetService.Tweet("Q1: How many unit tests does it take to make the sun go green?");
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+    }
+}
